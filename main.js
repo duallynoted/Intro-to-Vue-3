4f8b00f7e22a356a096918e40a586d4f1772b38a
Daniel Ridley <ridleydan31@gmail.com>
2025-01-24 22:59:06 -0600

    learned conditional rendering

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ const app = Vue.createApp({
       description: 'Knee high, warm, colorful foot skin casings.',
       image: './assets/images/socks_green.jpg',
       bombasUrl: 'https://www.bombas.com',
+      inventory: 0,
+      hasDesiredColorInStock: false,
+      onSale: getSaleDate(),
     };
   },
 });
+
+const getSaleDate = () => {
+  const baseDate = new Date();
+  const dayOfMonth = baseDate.getDate();
+  console.log('base date: ', baseDate.getDate());
+  return dayOfMonth;
+};
