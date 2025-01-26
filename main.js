88aafb0e5f2ea0af6641eca8d44dab032a9ffdaa
Daniel Ridley <ridleydan31@gmail.com>
2025-01-25 23:11:59 -0600

    used v-for for rendering lists

--- main.js
+++ main.js
@@ -5,9 +5,20 @@ const app = Vue.createApp({
       description: 'Knee high, warm, colorful foot skin casings.',
       image: './assets/images/socks_green.jpg',
       bombasUrl: 'https://www.bombas.com',
-      inventory: 0,
+      inventory: 100,
       hasDesiredColorInStock: false,
       onSale: getSaleDate(),
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue' },
+      ],
+      sizes: [
+        { id: 45559, size: 'Small', colors: ['green', ''] },
+        { id: 45565, size: 'Medium', colors: ['green', 'blue'] },
+        { id: 45571, size: 'Large', colors: ['', 'blue'] },
+        { id: 45585, size: 'Jumbo', colors: ['green', 'blue'] },
+      ],
     };
   },
 });
