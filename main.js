cf4fc02c5f25cfccac4fda38cdeb761e1c45ab91
Daniel Ridley <ridleydan31@gmail.com>
2025-01-27 22:29:25 -0600

    used v-on for event handling

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: 'Socks',
       description: 'Knee high, warm, colorful foot skin casings.',
-      image: './assets/images/socks_green.jpg',
+      image: './assets/images/socks_blue.jpg',
       bombasUrl: 'https://www.bombas.com',
       inventory: 100,
       hasDesiredColorInStock: false,
       onSale: getSaleDate(),
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'green' },
-        { id: 2235, color: 'blue' },
+        {
+          id: 2234,
+          color: 'Bright Green',
+          image: './assets/images/socks_green.jpg',
+        },
+        {
+          id: 2235,
+          color: 'Electric Blue',
+          image: './assets/images/socks_blue.jpg',
+        },
       ],
       sizes: [
         { id: 45559, size: 'Small', colors: ['green', ''] },
@@ -20,6 +29,18 @@ const app = Vue.createApp({
         { id: 45585, size: 'Jumbo', colors: ['green', 'blue'] },
       ],
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      console.log('cart: ', this.cart);
+      this.cart > 0 ? (this.cart -= 1) : null;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
 
